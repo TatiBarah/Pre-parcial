d4d96ec5d7183da4429372b4938359ea5800f637
TatiBarah <72039805+TatiBarah@users.noreply.github.com>
2020-10-14 21:24:51 -0600

    Add files via upload

---
+++ py-pre-parcial/clave_X/clave_x.py
@@ -0,0 +1,59 @@
+import math
+
+"""
+***************************************************************
+@@ ejercicio 1 @@
+un metodo python que haga la suma de 2 numeros
+2+4 = 6
+"""
+
+
+# start-->
+def suma():
+    numero1 = 2
+    numero2 = 4
+    return numero1 + numero2
+
+
+"""
+***************************************************************
+@@ ejercicio 2 @@
+un metodo python que haga la multiplicacion de 3 numeros
+2*4*5 = 40
+"""
+
+
+# start-->
+def multiplicacion():
+    numero1 = 2
+    numero2 = 4
+    numero3 = 5
+    return numero1 * numero2 * numero3
+
+
+"""
+***************************************************************
+@@ ejercicio 3 @@
+un metodo python que haga la suma de los numeros de la lista
+numerosLista = [2,5,4,6,9,12]
+"""
+
+
+# start-->
+def sumarLista():
+    numerosLista = [2, 5, 4, 6, 9, 12]
+    return sum(numerosLista)
+
+
+"""
+***************************************************************
+@@ ejercicio 4 @@
+colocar este proyecto en github
+colocar aca debajo la url
+enviar la url al correo balbino_a@hotmail.com
+"""
+
+
+# github url-->
+def getGithubUrl():
+    return "https://github.com/TatiBarah/Pre-parcial"
